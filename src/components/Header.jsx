58ea56ed57950b7b114b86388869535a10291066
Shivam seth <shivamseth221104@gmail.com>
2023-06-19 12:24:54 +0530

    final

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { useState} from "react";
 import { FaShoppingCart } from "react-icons/fa";
-import {BiSearch} from "react-icons/Bi";
+import {BiSearch} from "react-icons/bi";
 import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { MdShop } from "react-icons/md";
