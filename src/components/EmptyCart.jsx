eacf18c746597a75e8c116f0f505f785bb12f996
Shivam seth <shivamseth221104@gmail.com>
2023-06-22 01:31:42 +0530

    okk

--- src/components/EmptyCart.jsx
+++ src/components/EmptyCart.jsx
@@ -6,9 +6,9 @@ const EmptyCart =() =>{
         <div className="flex  justify-center gap-5">
         <div className="w-96"><img src={Emptymg} /></div>
         <div className="flex flex-col justify-center items-center">
-        <h1 className="text-5xl">Your Cart is Empty</h1>
+        <h1 className="text-5xl sm:text-3xl xsm:text-xl text-center">Your Cart is Empty</h1>
         <Link to="/Shop">
-        <button className="m-5 bg-slate-300 p-3 w-52">Shop Now!!</button>
+        <button className="m-5 bg-slate-300 p-3 w-52 xsm:w-16 xsm:text-xs sm:w-16 sm:text-xs">Shop Now!!</button>
         </Link>
         </div>
         </div>
